e466aceecd217058d1b22a7764884c763bf0738c
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2025-05-10 14:09:30 +0300

    branch created

--- dbms.sql
+++ dbms.sql
@@ -1,17 +1,17 @@
-create table Country(
-    country_id int primary key,
-    country_name varchar2(35)
+CREATE TABLE Country(
+    country_id INT PRIMARY KEY,
+    country_name VARCHAR2(35)
 );
 
-create table Release_date(
-    release_id int primary key,
-    release_date date
+CREATE TABLE Release_date(
+    release_id INT PRIMARY KEY,
+    release_date DATE
 );
 
-create table Cost(
-    cost_id int primary key,
+CREATE TABLE Cost(
+    cost_id INT PRIMARY KEY,
     cost_of NUMBER(10),
-    maintanance NUMBER(10),
+    maintenance NUMBER(10),
     operational NUMBER(10)
 );
 
@@ -19,20 +19,21 @@ CREATE TABLE Primary_Equipment (
     weapon_id INT PRIMARY KEY,
     weapon_type VARCHAR2(100),
     equipment_name VARCHAR2(100),
-    release_id int,
-    FOREIGN KEY (release_id) REFERENCES release_date(release_id)
+    release_id INT,
+    FOREIGN KEY (release_id) REFERENCES Release_date(release_id)
 );
 
-CREATE table Secondary_Equipment(
-    equipment_id NUMBER(10) primary key,
+CREATE TABLE Secondary_Equipment(
+    equipment_id NUMBER(10) PRIMARY KEY,
     equipment_type NUMBER(10),
-    equipment_name varchar2(100),
+    equipment_name VARCHAR2(100),
     release_id NUMBER(10)
 );
+
 CREATE TABLE Man_Equipment_Relation (
     mer_id INT PRIMARY KEY,
     equipment_id INT,
-    FOREIGN KEY (equipment_id) REFERENCES SECONDARY_EQUIPMENT(equipment_id)
+    FOREIGN KEY (equipment_id) REFERENCES Secondary_Equipment(equipment_id)
 );
 
 CREATE TABLE Infantry (
@@ -40,92 +41,90 @@ CREATE TABLE Infantry (
     mer_id INT,
     rank_soldier NUMBER(10),
     weapon_id INT,
-    FOREIGN KEY (mer_id) REFERENCES MAN_EQUIPMENT_RELATION(mer_id),
-    FOREIGN KEY (weapon_id) REFERENCES PRIMARY_EQUIPMENT(weapon_id)
+    FOREIGN KEY (mer_id) REFERENCES Man_Equipment_Relation(mer_id),
+    FOREIGN KEY (weapon_id) REFERENCES Primary_Equipment(weapon_id)
 );
-
 
 CREATE TABLE Inf_Suq_Rel (
-   isr_id INT primary key,
+   isr_id INT PRIMARY KEY,
    infantry_id INT,
-   FOREIGN KEY (infantry_id) REFERENCES INFANTRY(infantry_id)
+   FOREIGN KEY (infantry_id) REFERENCES Infantry(infantry_id)
 );
 
-create table Vehicle(
-    vehicle_id int primary key,
-    vehicle_name varchar2(100),
-    vehicle_type varchar2(100),
+CREATE TABLE Vehicle(
+    vehicle_id INT PRIMARY KEY,
+    vehicle_name VARCHAR2(100),
+    vehicle_type VARCHAR2(100),
     number_of_crew NUMBER(10),
-    release_id int REFERENCES release_date(release_id),
-    cost_id int REFERENCES cost(cost_id),
-    country_id int REFERENCES country(country_id)
+    release_id INT REFERENCES Release_date(release_id),
+    cost_id INT REFERENCES Cost(cost_id),
+    country_id INT REFERENCES Country(country_id)
 );
 
 CREATE TABLE Helicopter (
     vehicle_id NUMBER(10),
     Ground_Attack NUMBER(10),
     helicopter_speed NUMBER(10),
-    Stealth_Coefficent NUMBER(10),
+    Stealth_Coefficient NUMBER(10),
     CONSTRAINT heli_pk PRIMARY KEY (vehicle_id),
     CONSTRAINT heli_fk FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id)
 );
 
-create table Ship (
-    vehicle_id int not null primary key,
+CREATE TABLE Ship (
+    vehicle_id INT NOT NULL PRIMARY KEY,
     ads_power NUMBER(10),
-    balistic_power NUMBER(10) not null,
+    ballistic_power NUMBER(10) NOT NULL,
     ship_speed NUMBER(10),
     attack_range NUMBER(10),
-    CONSTRAINT ship_fk FOREIGN KEY (vehicle_id) REFERENCES vehicle(vehicle_id)
+    CONSTRAINT ship_fk FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id)
 );
 
 CREATE TABLE Tank (
     vehicle_id NUMBER(10),
-    Balistic_Power NUMBER(10),
-    Durabilty_Coefficent NUMBER(10),
+    Ballistic_Power NUMBER(10),
+    Durability_Coefficient NUMBER(10),
     Piercing_Power NUMBER(10),
     CONSTRAINT pk_tank PRIMARY KEY (vehicle_id),
     CONSTRAINT fk_tank_vehicle FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id)
 );
 
-create table Vehicle_Army_Relation (
-    var_id int not null primary key,
-    vehicle_id int not null,
-    CONSTRAINT var_fk REFERENCES vehicle(vehicle_id)
+CREATE TABLE Vehicle_Army_Relation (
+    var_id INT NOT NULL PRIMARY KEY,
+    vehicle_id INT NOT NULL,
+    CONSTRAINT var_fk FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id)
 );
 
 CREATE TABLE Squad (
-    squad_id int PRIMARY KEY,
+    squad_id INT PRIMARY KEY,
     squad_type VARCHAR2(100),
     squad_name VARCHAR2(100),
     release_date NUMBER(10),
-    isr_id int REFERENCES Inf_Suq_Rel(isr_id),
+    isr_id INT REFERENCES Inf_Suq_Rel(isr_id),
     squad_vehicle VARCHAR2(100),
     communication_range NUMBER(10),
     number_of_crew NUMBER(10),
     power_of_squad NUMBER(10)
 );
 
-CREATE TABLE  Squad_Army_Relation(
-    Sar_id NUMBER(10) PRIMARY KEY,
-    Squad_id NUMBER(10) REFERENCES SQUAD(Squad_id)
+CREATE TABLE Squad_Army_Relation(
+    sar_id NUMBER(10) PRIMARY KEY,
+    squad_id NUMBER(10) REFERENCES Squad(Squad_id)
 );
 
 CREATE TABLE ARMY (
-    Army_id NUMBER(10) PRIMARY KEY,
-    Army_Name VARCHAR2(100),
-    SAR_id NUMBER(10),
+    army_id NUMBER(10) PRIMARY KEY,
+    army_name VARCHAR2(100),
+    sar_id NUMBER(10),
     var_id NUMBER(10),
-    Linked_Unit VARCHAR2(100),
-    Country_id NUMBER(10),
-    Cost_id NUMBER(10),
+    linked_unit VARCHAR2(100),
+    country_id NUMBER(10),
+    cost_id NUMBER(10),
     release_id NUMBER(10),
-    number_Of_Crew NUMBER(10),
-
-    CONSTRAINT fk_army_sar FOREIGN KEY (SAR_id) REFERENCES Squad_Army_Relation(SAR_id),
+    number_of_crew NUMBER(10),
+    created_by_user NUMBER(10),
+    CONSTRAINT fk_army_sar FOREIGN KEY (sar_id) REFERENCES Squad_Army_Relation(sar_id),
     CONSTRAINT fk_army_var FOREIGN KEY (var_id) REFERENCES Vehicle_Army_Relation(var_id),
-    CONSTRAINT fk_army_country FOREIGN KEY (Country_id) REFERENCES Country(Country_id),
-    CONSTRAINT fk_army_cost FOREIGN KEY (Cost_id) REFERENCES Cost(Cost_id)
+    CONSTRAINT fk_army_country FOREIGN KEY (country_id) REFERENCES Country(country_id),
+    CONSTRAINT fk_army_cost FOREIGN KEY (cost_id) REFERENCES Cost(cost_id),
     CONSTRAINT fk_army_date FOREIGN KEY (release_id) REFERENCES Release_date(release_id)
-
 );
